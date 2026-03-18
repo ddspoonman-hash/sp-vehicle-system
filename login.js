7998e3087610e68d639e7de416a6706c8c0bf27a
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-18 20:27:18 +0900

    Update login.js

--- login.js
+++ login.js
@@ -20,3 +20,33 @@ localStorage.setItem("user",JSON.stringify(user));
 location.href="driver_start.html";
 
 }
+
+function login(){
+
+const id = document.getElementById("id").value;
+const pass = document.getElementById("pass").value;
+
+fetch(GAS+"?type=drivers")
+.then(r=>r.json())
+.then(list=>{
+
+const user = list.find(u=>u.id==id && u.pass==pass);
+
+if(!user){
+alert("IDまたはPASS違う");
+return;
+}
+
+// 保存
+localStorage.setItem("user",JSON.stringify(user));
+
+// ★ 管理者分岐（ここ重要）
+if(user.id==="admin"){
+location.href="admin.html";
+}else{
+location.href="driver_start.html";
+}
+
+});
+
+}
