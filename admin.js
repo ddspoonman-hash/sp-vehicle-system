ad79c98640167bc2f1f12f21da46b7978056e112
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-18 14:08:29 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,20 +2,26 @@ const GAS="https://script.google.com/macros/s/AKfycbxkgNmKdoeilTzXtelG_1VZNu8MHP
 
 window.onload=async()=>{
 
-const cars=await fetch(GAS+"?type=cars").then(r=>r.json());
+const init=await fetch(GAS+"?type=init").then(r=>r.json());
 
-cars.forEach(c=>{
-const opt=document.createElement("option");
-opt.value=c;
-opt.textContent=c;
-car.appendChild(opt);
+init.cars.forEach(c=>{
+const o=document.createElement("option");
+o.value=c;
+o.textContent=c;
+car.appendChild(o);
+});
+
+// 現在位置表示
+map.innerHTML="";
+init.running.forEach(r=>{
+map.innerHTML+=`${r.car}:${r.lat},${r.lng}<br>`;
 });
 
 };
 
-async function updateMeter(){
+function updateMeter(){
 
-await fetch(GAS,{
+fetch(GAS,{
 method:"POST",
 body:JSON.stringify({
 type:"updateMeter",
