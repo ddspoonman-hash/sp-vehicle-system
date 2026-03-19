96628c1b73803625a388d88ca473e01777436283
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-19 16:50:20 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -75,3 +75,32 @@ load();
 
 // 初期ロード
 load();
+
+
+let map;
+
+async function load(){
+
+const data = await fetch(GAS+"?type=init").then(r=>r.json());
+
+// 地図初期化
+map = new google.maps.Map(document.getElementById("map"),{
+zoom:12,
+center:{lat:35.0,lng:136.0}
+});
+
+// マーカー表示
+data.running.forEach(r=>{
+
+new google.maps.Marker({
+position:{
+lat:Number(r.lat),
+lng:Number(r.lng)
+},
+map:map,
+title:r.car+" "+r.driver
+});
+
+});
+
+}
