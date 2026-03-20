a89bdde07c38b417ef6deb0b6d0bf2bc0a5b6080
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-20 22:05:43 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,33 +1,33 @@
 const GAS="https://script.google.com/macros/s/AKfycbwbMFxKiQlT_hpb_iNjljeEvKZ7LMr9q8i2KpdW6iWrO6d3pv40iun7SLRTFAstn9C5/exec";
 
-const GAS="https://script.google.com/macros/s/AKfycbwbMFxKiQlT_hpb_iNjljeEvKZ7LMr9q8i2KpdW6iWrO6d3pv40iun7SLRTFAstn9C5/exec";
-
 function login(){
 
 const id = document.getElementById("id").value;
 const pass = document.getElementById("pass").value;
 
-// 毎回callback名変える（超重要）
-const cb = "cb_" + Date.now();
+const script = document.createElement("script");
 
-window[cb] = function(list){
+script.src = GAS + "?type=drivers&callback=handleLogin";
+
+document.body.appendChild(script);
+
+window.handleLogin = function(list){
 
 const user = list.find(u=>u.id==id && u.pass==pass);
 
 if(!user){
-alert("ログイン失敗");
+alert("IDまたはPASS違う");
 return;
 }
 
 localStorage.setItem("user",JSON.stringify(user));
 
-location.href = user.id==="admin" ? "admin.html" : "driver_start.html";
+if(user.id==="admin"){
+location.href="admin.html";
+}else{
+location.href="driver_start.html";
+}
 
 };
-
-// JSONP
-const script = document.createElement("script");
-script.src = GAS + `?type=drivers&callback=${cb}&t=${Date.now()}`;
-document.body.appendChild(script);
 
 }
