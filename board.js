b14c04e5b6c2cb6ff3956b24f240a145df2aea3a
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-23 09:29:11 +0900

    Update board.js

--- board.js
+++ board.js
@@ -1,25 +1,32 @@
 const GAS="https://script.google.com/macros/s/AKfycbxkgNmKdoeilTzXtelG_1VZNu8MHP0wxxkPNLaS-OY4Ix2V08bxJx7CyYMlozKyirLN/exec";
 
+function jsonp(url){
+return new Promise(res=>{
+const cb="cb_"+Date.now();
+window[cb]=data=>{
+res(data);
+delete window[cb];
+};
+const s=document.createElement("script");
+s.src=url+"&callback="+cb+"&t="+Date.now();
+document.body.appendChild(s);
+});
+}
+
 async function load(){
-
-let d=await (await fetch(GAS+"?type=board")).json();
-
+let d=await jsonp(GAS+"?type=board");
 list.innerText=d.map(x=>x.msg).join("\n");
-
 }
 
 async function post(){
-
-await fetch(GAS,{
-method:"POST",
-body:JSON.stringify({
-type:"board",
-msg:msg.value
-})
-});
-
+await jsonp(GAS+`?type=board&msg=${encodeURIComponent(msg.value)}`);
+msg.value="";
 load();
+}
 
+function logout(){
+localStorage.clear();
+location.href="index.html";
 }
 
 load();
