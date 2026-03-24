80647c4ab9aecf4a5f135d8d0b2db40047d8e785
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-24 16:24:09 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -82,6 +82,22 @@ alert("更新OK");
 load();
 }
 
+async function loadGroups(){
+
+const groups = await jsonp(GAS+"?type=groups");
+
+pGroup.innerHTML="";
+
+groups.forEach(g=>{
+const o=document.createElement("option");
+o.value=g;
+o.textContent=g;
+pGroup.appendChild(o);
+});
+
+}
+
+
 window.onload=()=>{
 initMap();
 load();
