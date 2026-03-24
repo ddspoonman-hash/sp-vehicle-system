2ccaeaa14515d89bb09a05e679a4af89d64ad65a
ddspoonman-hash <dd.spoonman@gmail.com>
2026-03-24 12:33:43 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,26 +1,22 @@
 const GAS="https://script.google.com/macros/s/AKfycbwbMFxKiQlT_hpb_iNjljeEvKZ7LMr9q8i2KpdW6iWrO6d3pv40iun7SLRTFAstn9C5/exec";
 
 function login(){
+  const id = document.getElementById("id").value;
+  const pass = document.getElementById("pass").value;
 
-const id = document.getElementById("id").value;
-const pass = document.getElementById("pass").value;
+  window.handleLogin = function(list){
+    const user = list.find(u=>u.id==id && u.pass==pass);
 
-window.handleLogin = function(list){
+    if(!user){
+      alert("IDまたはPASS違う");
+      return;
+    }
 
-const user = list.find(u=>u.id==id && u.pass==pass);
+    localStorage.setItem("user",JSON.stringify(user));
+    location.href="driver_start.html";
+  };
 
-if(!user){
-alert("IDまたはPASS違う");
-return;
-}
-
-localStorage.setItem("user",JSON.stringify(user));
-
-location.href = user.id==="admin" ? "admin.html" : "driver_start.html";
-};
-
-const script = document.createElement("script");
-script.src = GAS + "?type=drivers&callback=handleLogin&t=" + Date.now();
-document.body.appendChild(script);
-
+  const script = document.createElement("script");
+  script.src = GAS + "?type=drivers&callback=handleLogin&t=" + Date.now();
+  document.body.appendChild(script);
 }
